ff1d4844f298999713e10e15119b111e8b2e2d40
elbayoumi <mohamedashrafelbayoumi.com>
2023-03-01 02:41:40 +0200

    v1

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AcceptsOrdersComponent } from './accepts-orders/accepts-orders.component';
+import { RejectsOrdersComponent } from './rejects-orders/rejects-orders.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { OnDeliverComponent } from './on-deliver/on-deliver.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    OrdersComponent,
+    AcceptsOrdersComponent,
+    RejectsOrdersComponent,
+    LoginComponent,
+    OnDeliverComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+RouterModule,
+BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
