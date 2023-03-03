84563ea183de3ff83e28a010bea3a534daccb02a
elbayoumi <mohamedashrafelbayoumi.com>
2023-03-04 00:55:48 +0200

    v1.1.0

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators ,FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,47 +8,72 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  loginForm!:FormGroup;
+  // loginForm!:FormGroup;
   submitted = false;
-  userName: any;
-
+  // userName: any;
+errorPass:string=""
+errorUserName:string=''
   constructor(private fb:FormBuilder,private router:Router){}
 
-  ngOnInit(): void
-  {
-    this.loginForm = this.fb.group({
-      userName:['',[Validators.required,Validators.min(4)]],
-      passWord:['',[Validators.required,Validators.pattern(
-        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
-// '^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$' for email
-      )]]
-    });
+  // ngOnInit(): void
+  // {
+  //   this.loginForm = this.fb.group({
+  //     userName:['',[Validators.required]],
+  //     passWord:['',[Validators.required,Validators]]
+  //   });
+  // }
+
+  // get loginData(){
+  //   return this.loginForm.controls;
+  // }
+
+  // onSubmit()
+  // {
+  //      this.submitted = true;
+  //      if(this.loginForm.valid)
+  //      {
+  //        localStorage.setItem("userName",this.loginForm.get('userName')?.value);
+
+  //       //  localStorage.setItem("token",this.userName?.value);
+
+  //        localStorage.setItem("passWord",this.loginForm.get('passWord')?.value);
+
+  //         this.clear();
+  //         this.loginForm.disable();
+  //         this.goTo('orders')
+
+  //         console.log(this.userName)
+  //      }
+
+  // }
+
+  // clear()
+  // {
+  //   this.loginForm.patchValue({
+  //     userName:'',
+  //     passWord:''
+  //   });
+  // }
+  // goTo(param:any) {
+
+  //   this.router.navigate([param])
+  //   }
+  myForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(7)]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    if(this.myForm.touched || this.myForm.invalid){
+      this.errorPass="Password is required";
+      this.errorUserName="User name is required";
+
+    }else if (this.myForm.touched && this.myForm.valid)
+    this.goTo("orders")
+  }
+  goTo(props:string){
+    this.router.navigate([props])
   }
 
-  get loginData(){
-    return this.loginForm.controls;
-  }
-
-  onSubmit()
-  {
-       this.submitted = true;
-       if(this.loginForm.valid)
-       {
-          localStorage.setItem("userName",this.loginForm.get('userName')?.value);
-          localStorage.setItem("passWord",this.loginForm.get('passWord')?.value);
-          this.clear();
-          this.loginForm.disable();
-          this.router.navigate(['orders',this.userName]);
-          console.log(this.userName)
-       }
-
-  }
-
-  clear()
-  {
-    this.loginForm.patchValue({
-      userName:'',
-      passWord:''
-    });
-  }
 }
+
