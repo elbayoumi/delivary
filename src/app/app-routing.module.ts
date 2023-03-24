3870407167a24949a26256e088a4c077ca718f21
elbayoumi <mohamedashrafelbayoumi.com>
2023-03-24 19:16:18 +0200

    v2.5.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
       { path: '', redirectTo: 'orders', pathMatch: 'full' },
-      { path: 'orders', title: 'wating orders', component: OrdersComponent },
+      { path: 'orders', title: 'Wating orders', component: OrdersComponent },
 
       //       {
       //         path: '',
